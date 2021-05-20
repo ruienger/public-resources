90031e18bf0186d993c885036a7952aba38bb18f
ruienger <13643610935@163.com>
2021-05-20 13:07:57 +0800

    typeof.js

--- JS/typeof.js
+++ JS/typeof.js
@@ -1,10 +1,26 @@
 /*
  * @Date: 2021-05-19 14:15:10
  * @Editors: HouZekong
- * @LastEditTime: 2021-05-19 14:15:52
+ * @LastEditTime: 2021-05-19 15:46:01
  * @FilePath: \personalStudy\public-resources\JS\typeof.js
  * @Description: typeof 用于存储判断类型的各个方法
  */
-export function isObj(tar) {
-    return Object.prototype.toString.call(tar) === "[object Object]";
+export function isObj(test) {
+    return Object.prototype.toString.call(test) === "[object Object]"
+}
+// 判断是否为数组
+export function isArr(test) {
+    return Array.isArray(test)
+}
+// 判断是否为函数
+export function isFun(test) {
+    return typeof test === 'function'
+}
+// 判断类型是否一致
+export function isTypeSame(tar, val) {
+    return getType(tar) === getType(val)
+}
+// 获取类型
+export function getType(tar) {
+    return isObj(tar) ? "object" : isArr(tar) ? "array" : typeof tar
 }
